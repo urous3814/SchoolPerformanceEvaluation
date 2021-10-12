2516a3adfc0b42551730394605004868422ed40f
UROUS3814 <40907210+urous3814@users.noreply.github.com>
2021-10-12 15:50:23 +0900

    Update pymain.py

--- pymain.py
+++ pymain.py
@@ -2,20 +2,22 @@ import serial
 import turtle as tu
 
 
-arduino_port = input("통신 포트 이름: ")
+#arduino_port = input("통신 포트 이름: ")
 
-serialFromArduino = serial.Serial(arduino_port, 9600, timeout=1)
+#serialFromArduino = serial.Serial(arduino_port, 9600, timeout=1)
 t = tu.Pen()
+t.speed(300)
 
 while True:
-    line = serialFromArduino.readline().decode("utf-8")
-    print(line)
-    d = int(line)
+    #line = serialFromArduino.readline().decode("utf-8")
+    # print(line)
+    # d = int(line)
+    d = 30
     if(d>0 and d<60):
         t.penup()
-        t.forward(d*30)
+        t.forward(d*15)
         t.begin_fill()
-        t.circle(30)
+        t.circle(15)
         t.end_fill()
-        t.backward(d*30)
+        t.backward(d*15)
         t.left(5)
