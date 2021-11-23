a83e94095be73a9e1956ee194118fcbc935f6dac
UROUS3814 <40907210+urous3814@users.noreply.github.com>
2021-11-23 15:33:44 +0900

    Update arduino_main.ino

--- arduino_main.ino
+++ arduino_main.ino
@@ -1,18 +1,49 @@
 #include <SoftwareSerial.h>
-int blueTx=0;   //Tx (보내는핀 설정)
-int blueRx=1;   //Rx (받는핀 설정)
-SoftwareSerial mySerial(blueTx, blueRx);  //시리얼 통신을 위한 객체선언
+#include <Servo.h> 
+#define blueTx 0   //Tx (보내는핀 설정)
+#define blueRx 1   //Rx (받는핀 설정)
+#define TRIG 10
+#define ECHO 8
+int servoPin=9;
+SoftwareSerial BTSerial(blueTx, blueRx);  //시리얼 통신을 위한 객체선언
+Servo servo; 
+int ang = 0, dis = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);   //시리얼모니터
-  mySerial.begin(9600); //블루투스 시리얼
+  BTSerial.begin(9600); //블루투스 시리얼
+  servo.attach(servoPin); 
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
 }                                                       //블루투스에 문제가 있음
+
+int pulse(){
+  long duration, distance;
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  duration = pulseIn (ECHO, HIGH);
+  distance = duration * 17 / 1000; 
+  return(distance);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(BTSerial.available())                     // BTSerial에 입력이 되면
       Serial.write(BTSerial.read());           // BTSerial에 입력된 값을 시리얼 모니터에 출력
-    if(Serial.available())                     // 시리얼 모니터에 입력이 되면
+   if(Serial.available())                     // 시리얼 모니터에 입력이 되면
       BTSerial.write(Serial.read());           // 그 값을 BTSerial에 출력
+
+   //--------------------------------------
+
+/*   for(ang; ang>360; ang)
+   {
+      servo.write(ang);
+      dis = pulse();
+      BTSerial.write("@"+ang+"#"+dis);
+            
+   }*/
 }
