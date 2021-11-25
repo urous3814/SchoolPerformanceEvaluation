81806a56ab0b43c1e7f309643902c16cc4b62212
UROUS3814 <40907210+urous3814@users.noreply.github.com>
2021-11-25 11:06:43 +0900

    Update arduino_main.ino

--- arduino_main.ino
+++ arduino_main.ino
@@ -1,24 +1,24 @@
 #include <SoftwareSerial.h>
-#include <Servo.h> 
-#define blueTx 0   //Tx (보내는핀 설정)
-#define blueRx 1   //Rx (받는핀 설정)
+#include <Stepper.h>
+#define blueTx 2   //Tx (보내는핀 설정)
+#define blueRx 3   //Rx (받는핀 설정)
 #define TRIG 10
 #define ECHO 8
-#define servoPin 9
+const int stepsPerRevolution = 57;
 SoftwareSerial BTSerial(blueTx, blueRx);  //시리얼 통신을 위한 객체선언
-Servo servo; 
+Stepper myStepper(stepsPerRevolution, 4, 5, 6, 7);
 int ang = 0, dis = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);   //시리얼모니터
   BTSerial.begin(9600); //블루투스 시리얼
-  servo.attach(servoPin); 
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
-}                                                       
+  myStepper.setSpeed(100);
+}
 
-int pulse(){
+int pulse() {
   long duration, distance;
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
@@ -26,22 +26,22 @@ int pulse(){
   delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
   duration = pulseIn (ECHO, HIGH);
-  distance = duration * 17 / 1000; 
-  return(distance);
+  distance = duration * 17 / 1000;
+  return (distance);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-//   if(BTSerial.available())                     // BTSerial에 입력이 되면
-      for(ang = 0; ang<360; ang+=5)
-      {
-        servo.write(ang);
-        dis = pulse();
-        String data = (String(ang)+"_"+String(dis));
-        BTSerial.println(data);       
-        Serial.println(data);
-        delay(100); 
-      }
+  //   if(BTSerial.available())                     // BTSerial에 입력이 되면
+  for (ang = 0; ang < 360; ang += 10)
+  {
+    myStepper.step(stepsPerRevolution);
+    dis = pulse();
+    String data = (String(ang) + "_" + String(dis));
+    BTSerial.println(data);
+    Serial.println(data);
+  }
+  myStepper.step(-2048);
   delay(1000);
 }
