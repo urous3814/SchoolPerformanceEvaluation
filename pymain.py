16617003c6a22f3a7f077b3ac7bb10f0446bc294
UROUS3814 <40907210+urous3814@users.noreply.github.com>
2021-12-02 02:24:02 +0900

    Update pymain.py

--- pymain.py
+++ pymain.py
@@ -3,7 +3,7 @@ import turtle as tu #turtle, 파이썬의 대표적인 기초 그래픽 모델
 while True:         #시리얼 통신 연결
     arduino_port = input("통신 포트 이름: ")
     try:
-        serialFromArduino = serial.Serial(arduino_port, 9600, timeout=None)  #설정한 통신 포트로 시리얼 통신 형성
+        ard_serial = serial.Serial(arduino_port, 9600, timeout=None)  #설정한 통신 포트로 시리얼 통신 형성
     except:
         continue
     else: 
@@ -16,9 +16,9 @@ t = tu.Pen()    #터틀의 펜 선언
 t.speed(300)    #펜의 속도 설정
 
 while True:
-    if serialFromArduino.in_waiting == 0 : #캐시에 받은 바이트 수
+    if ard_serial.in_waiting == 0 : #캐시에 받은 바이트 수
         pass
-    line = serialFromArduino.readline().decode("utf-8") #문자열을 받아와 utf-8로 디코딩
+    line = ard_serial.readline().decode("utf-8") #문자열을 받아와 utf-8로 디코딩
     print(line)
     ang, dis = line.split("_")          #line = 각도_거리
     ang = int(ang)  #각도
